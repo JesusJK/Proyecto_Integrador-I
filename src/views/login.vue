9a1deee0642a9d2c1f6618f30c98eb70a2023ea9
JesusJK <camilojesus19@gmail.com>
2024-05-09 16:37:08 -0500

    Enrutamiento listo

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,49 +1,71 @@
 <script setup>
-import {  RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import { ref } from 'vue';
 
 const valor = ref(true);
-const rol = "";
-function toggleInicio(asign_rol) {
-    valor.value=false;
-    rol.value="Como" + asign_rol;
-    
+const rol = ref("");
+const valor2 = ref(false);
+
+function elInicio(asignrol) {
+    valor.value = false;
+    rol.value = "Como " + asignrol;
+    valor2.value = true;
+
 }
+console.log(valor);
 </script>
 
 <template>
+
     <body class="bg-[#6E9ABB]">
-    <div class=" h-max bg-[#0C2733] text-[#0C2733]">0</div>
-    <div class="container mx-auto py-[300px] ">
-      <div class="cuadro max-w-md mx-auto bg-[#8C7F6C] rounded-lg shadow-md overflow-hidden ">
-        <div class="px-20 py-20">
-          <div class="text-center mb-8">
-            <h2 class="text-xl font-semibold text-white">
-              Iniciar Sesion
-            </h2>
-          </div>
-          <div class="flex flex-col space-y-4">
+        <div class=" h-max bg-[#0C2733] text-[#0C2733]">0</div>
+        <div class="container mx-auto py-[400px] ">
+            <div class="cuadro max-w-md mx-auto bg-[#8C7F6C] rounded-lg shadow-md overflow-hidden ">
+                <div class="px-20 py-20">
+                    <div class="mb-5"> 
+                        <h2 class="text-xl font-semibold text-white text-center">
+                            Iniciar Sesión
+                        </h2>
+                        <p class="text-[#0C2733] font-semibold text-center m-1">
+                            {{ rol }}
+                        </p>
+                    </div>
 
-            <a href="#" v-if="valor" @click="toggleInicio('Estudiante')"
-              class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
-              Estudiante
-            </a>
+                    <div class="flex flex-col space-y-4">
+
+                        <a href="#" v-if="valor" @click="elInicio('Estudiante')"
+                            class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+                            Estudiante
+                        </a>
 
 
-            <a href="#" v-if="valor" @click="toggleInicio('Experto')"
-              class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
-              Experto
-            </a>
+                        <a href="#" v-if="valor" @click="elInicio('Experto')"
+                            class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+                            Experto
+                        </a>
 
-            <a href="#" v-if="valor" @click="toggleInicio('Desarrollador')"
-              class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
-              Desarrollador
-            </a>
+                        <a href="#" v-if="valor" @click="elInicio('Desarrollador')"
+                            class="bg-[#B0966F] hover:shadow-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center">
+                            Desarrollador
+                        </a>
 
-          </div>
+                        <p class="text-center">
+                            <input type="email" name="" id="" placeholder="Correo"
+                            class="mt-8 border-b-2 border-gray-300 p-1 outline-none" v-if="valor2">
+                            <input type="password" name="" placeholder="Contraseña"
+                            class="mt-8 border-b-2 border-gray-300 p-1 outline-none" v-if="valor2">
+                            <br>
+                            <br>
+                            <RouterLink to="/form" class="font-semibold text-xl">
+                               Iniciar
+                            </RouterLink>
+                        </p>
+                            
+
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class=" h-max bg-[#0C2733] text-[#0C2733]">0</div>   
-  </body>
+        <div class=" h-max bg-[#0C2733] text-[#0C2733]">0</div>
+    </body>
 </template>
